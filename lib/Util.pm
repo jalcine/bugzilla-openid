70fcd50d1545d1b38188785d9536da8eab75400e
Jacky Alcine <jackyalcine@gmail.com>
2012-04-25 23:12:51 -0400

    Updated lib/Util.pm with a convienent method; get_page.

--- lib/Util.pm
+++ lib/Util.pm
@@ -28,5 +28,55 @@
 package Bugzilla::Extension::OpenID::Util;
 use strict;
 use warnings;
+use Bugzilla;
+use base qw(Bugzilla::Auth::Login);
+
+# There's three major phases; register, authenticate and add.
+#
+# register     - Registers a new OpenID account.
+# authenticate - Handles the authentication work for OpenID.
+# add          - Adds a OpenID identity to the user account.
+#
+# In order that redirects are properly setup, there's a redirection
+# page that pushes the user to the needed location, and providing
+# a bit more information about said redirection.
+#
+sub get_page {
+    my ($page_id) = @_;
+    my $url = correct_urlbase() + "/page.cgi?id=";
+
+    switch ($page_id){
+        case "handle_register":
+            $url += "openid_continue.html&mode=register";
+        break;
+
+        case "handle_authenticate"
+            $url += "openid_continue.html&mode=authenticate";
+        break;
+
+        case "handle_add"
+            $url += "openid_continue.html&mode=add";
+        break
+
+        case "handle_add":
+            $url += "openid_add.html";
+        break;
+
+        case "handle_register":
+            $url += "openid_register.html";
+        break;
+
+        case "handle_authenticate"
+            $url += "openid_authenticate.html";
+        break;
+
+        default:
+            # TODO: Should we throw an error?
+            $url = correct_urlbase();
+        break;
+    }
+
+    return $url;
+}
 
 1;
